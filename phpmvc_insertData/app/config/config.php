2bca7c8a919a59fd0e034cf7664639cd84283194
Ivan Bayu Pratama <121966880+ivanbayupratama@users.noreply.github.com>
2024-10-28 21:04:13 +0700

    Add files via upload

---
+++ phpmvc_insertData/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("BASEURL", "http://localhost/belajar/phpmvc_insertData/public");
+
+// DATANASE
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'php_mvc');
---
+++ phpmvc_insertData/app/init.php
@@ -0,0 +1,12 @@
+<?php
+
+// Menginisiasi
+
+// Core adalah classnya
+require_once 'core/App.php';
+// controler ini utama, controllers extend
+require_once 'core/Controller.php';
+// database
+require_once 'core/Database.php';
+// Konstanta
+require_once 'config/config.php';
