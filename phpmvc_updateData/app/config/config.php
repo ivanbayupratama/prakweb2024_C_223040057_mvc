c0a2fdc6c2fc5498ff9d14338fa87f6d677a370c
Ivan Bayu Pratama <121966880+ivanbayupratama@users.noreply.github.com>
2024-10-28 22:46:01 +0700

    Add files via upload

---
+++ phpmvc_updateData/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("BASEURL", "http://localhost/phpmvc_updateData/public");
+
+// DATANASE
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'php_mvc');
---
+++ phpmvc_updateData/app/models/User_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class User_model
+{
+
+
+    private $nama = "Ivan Bayu";
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
