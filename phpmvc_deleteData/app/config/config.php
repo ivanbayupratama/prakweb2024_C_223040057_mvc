f39ac4a93ae34b649c73b87a7571f5e674a0723b
Ivan Bayu Pratama <121966880+ivanbayupratama@users.noreply.github.com>
2024-10-28 22:47:55 +0700

    Add files via upload

---
+++ phpmvc_deleteData/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("BASEURL", "http://localhost/phpmvc_deleteData/public");
+
+// DATABASE
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'php_mvc');
